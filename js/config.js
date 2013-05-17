bf553e12bdbd966d6e0200f678753de5c37b74c5
Lindsay Thomas <lindsay_thomas@esri.com>
2013-05-17 14:43:00 -0700

    Updated basemaps

--- js/config.js
+++ js/config.js
@@ -82,6 +82,11 @@ dojo.declare("js.Config", null, {
         ThumbnailSource: "images/parcel.png",
         Name: "Streets",
         MapURL: "http://arcgis-tenone2012-1974758903.us-west-1.elb.amazonaws.com/arcgis/rest/services/ParcelPublicAccessMI/MapServer"
+    },{
+        Key: "imageryMap",
+        ThumbnailSource: "images/parcel.png",
+        Name: "Imagery",
+        MapURL: "http://arcgis-tenone2012-1974758903.us-west-1.elb.amazonaws.com/arcgis/rest/services/ImageryHybrid/MapServer"
     }],
 
     // Initial map extent. Use comma (,) to separate values and don t delete the last comma
